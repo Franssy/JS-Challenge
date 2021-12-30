d9ddc71a701af726caebd9b8544b370b4212633e
Franssy <94227138+Franssy@users.noreply.github.com>
2021-12-30 10:21:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,224 @@
+// 1. Declare two variables: firstName and lastName. 
+
+let firstName = "kehinde"
+let lastName = "Otedola"
+
+// 2. Copy the value from firstName AND lastName to boss. i.e variable  boss should be equal to Kehinde Odetola
+
+
+boss = "kehinde" + " " + "Otedola"
+
+// 3. Show the value of boss using alert
+alert(boss)
+
+
+
+// 4. Create a variable with the name of our planet. How would you name such a variable? 
+
+let Planet = "earth"
+
+ 
+// 5. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+let currentUserName = "Francis"
+
+
+// 6. create an array arr of 20 items in it
+
+let christmasList = ['AC','light bulb','trees','flowers','cards','decorations','clothes','sofas', 'dining table','shoes','rice','tomatoes','spices','Chicken','Beef','salad cream','CatchUp','letus','sweet potatoes','carrot'];
+
+
+// 7. output the items with positions in a  multiple of 5  in  array  arr  
+
+console.log(christmasList[4])
+console.log(christmasList[9])
+console.log(christmasList[14])
+console.log(christmasList[19])
+
+// 8. create an dog Object with 4 properties.
+
+let floki = {
+    age: 2,
+    breed:"Shiba Inu",
+    likesBarking:true,
+    gender:"male"
+}
+
+console.log(floki)
+
+// 9. create an array of objects of 8  different animals and output 1 of each animal's properties
+
+let flokiDoge = {
+    age: 2,
+    breed:"Shiba Inu",
+    likesBarking:true,
+    gender:"male"
+}
+
+console.log(breed)
+
+
+let dogeFloki = {
+    age: 2,
+    breed:"Shiba Inu",
+    likesBarking:true,
+    gender:"male"
+}
+
+console.log(age)
+
+
+
+let shiba = {
+    age: 2,
+    breed:"Shiba Inu",
+    likesBarking:true,
+    gender:"male"
+}
+
+console.log(gender)
+
+
+// let floki = {
+//     age: 2,
+//     breed:"Shiba Inu",
+//     likesBarking:true,
+//     gender:"male"
+// }
+
+// console.log(floki)
+
+
+// let floki = {
+//     age: 2,
+//     breed:"Shiba Inu",
+//     likesBarking:true,
+//     gender:"male"
+// }
+
+// console.log(floki)
+
+
+// let floki = {
+//     age: 2,
+//     breed:"Shiba Inu",
+//     likesBarking:true,
+//     gender:"male"
+// }
+
+// console.log(floki)
+
+
+
+// let floki = {
+//     age: 2,
+//     breed:"Shiba Inu",
+//     likesBarking:true,
+//     gender:"male"
+// }
+
+// console.log(floki)
+
+
+// let floki = {
+//     age: 2,
+//     breed:"Shiba Inu",
+//     likesBarking:true,
+//     gender:"male"
+// }
+
+// console.log(floki)
+
+// 10. Create a variable for ptpAmount and write a conditional statement using if and else for different cases of the PTP Amount; Have an output for any possible amount. (be creative here for the possible scenarios of PTP). 
+
+let ptpAmount = 20000
+
+if (ptpAmount === 20000){
+console.log("Congratulations, You Made Him to BFree of Debt")
+}
+
+else if(ptpAmount === 10000){
+    console.log("No Payment")
+}
+
+else if (ptpAmount === 21000){
+    console.log("Client OverPaid")
+}
+
+else if(ptpAmount === 4500){
+    console.log("Payment is below Average Amount Agreed")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // let all = 12+10
+// // console.log(all)
+
+// // all = 20+20
+// // console.log(all)
+
+
+// let Name = "kinsley"
+
+// if (Name === "Francis"){
+//     console.log("Got Ya")
+// }
+
+// else if(Name === "Ngozi"){
+//     console.log("Got Ya")
+// }
+
+// else if(Name === "Kingsley"){
+//     console.log("Got Ya")
+// }
+
+// else if(Name === "Emeka"){
+//     console.log("Not Found On Record")
+// }
+
+
+
+
+
+// // console.log("Nothing Here")
+// // console.log("Nothing Here")
+// // console.log("Nothing Here")
+// // console.log("Nothing Here")
+
+
+// // let age = 12;
+// // let message = "Nothing Here"
